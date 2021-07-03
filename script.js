91d9654d75781b28b10e95055686b26aac1b824b
Amitrajit Das <amitrajitdas31@gmail.com>
2021-07-03 22:22:37 +0530

    updated

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function replaceText(element) {
   if (element.hasChildNodes()) {
     element.childNodes.forEach(replaceText)
   } else if (element.nodeType === Text.TEXT_NODE) {
-    if (element.textContent.match(/coronavirus/gi)) {
+    if (element.textContent.match(/mlh/gi)) {
       const newElement = document.createElement('span')
-      newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi, '<span class="rainbow">$1</span>')
+      newElement.innerHTML = element.textContent.replace(/(mlh)/gi, '<span class="rainbow">$1</span>')
       element.replaceWith(newElement)
     }
   }
